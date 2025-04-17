fce6c615acc02deea020e78f4942b03f82e88a70
Agudelo <agudelo.jhoan.4904@eam.edu.co>
2025-04-17 15:24:56 -0500

    Primeros archivos

---
+++ src/main/java/com/teleCofe/TeleCofe/TeleCofeApplication.java
@@ -0,0 +1,13 @@
+package com.teleCofe.TeleCofe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleCofeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleCofeApplication.class, args);
+	}
+
+}

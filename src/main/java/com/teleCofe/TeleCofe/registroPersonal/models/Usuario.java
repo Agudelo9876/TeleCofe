d610671bc53d61c01ecb81e0dab05464908f7bcc
Agudelo <agudelo.jhoan.4904@eam.edu.co>
2025-04-17 14:46:15 -0500

    Primer commit

---
+++ src/main/java/com/teleCofe/TeleCofe/registroPersonal/models/Usuario.java
@@ -0,0 +1,11 @@
+package com.teleCofe.TeleCofe.registroPersonal.models;
+
+public class Usuario{
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Integer age;
+    private String phone;
+    
+}
---
+++ src/test/java/com/teleCofe/TeleCofe/TeleCofeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleCofe.TeleCofe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleCofeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
